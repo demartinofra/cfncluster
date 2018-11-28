4b9556fd4b6b096e1b574728695e8c52c73b3093
Francesco De Martino <fdm@amazon.com>
2018-11-28 10:59:56 +0100

    Add ShellCheck to TravisCI build
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cli/pcluster/examples/job.sh
+++ cli/pcluster/examples/job.sh
@@ -2,4 +2,4 @@
 
 echo "Starting Job"
 /usr/lib64/openmpi/bin/mpirun --allow-run-as-root -np 2 sleep 10
-echo $(ls -la)
+ls -la
--- cli/pcluster/resources/batch/docker/upload-docker-images.sh
+++ cli/pcluster/resources/batch/docker/upload-docker-images.sh
@@ -2,14 +2,14 @@
 set -eu
 
 push_docker_image() {
-    local image=$1
+    local image_tag=$1
     echo "Uploading image image"
-    docker tag "${IMAGE_REPO_NAME}:${image}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${image}"
-    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${image}"
+    docker tag "${IMAGE_REPO_NAME}:${image_tag}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${image_tag}"
+    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${image_tag}"
 }
 
 if [ -z "${IMAGE}" ]; then
-    for file in $(find `pwd` -type f -name Dockerfile); do
+    for file in $(find $(pwd) -type f -name Dockerfile); do
         IMAGE_TAG=$(dirname "${file}" | xargs basename)
         push_docker_image "${IMAGE_TAG}"
     done
