eb749d8595b76e75503a5d3f7cdfccecc1480302
Francesco De Martino <fdm@amazon.com>
2020-01-29 14:40:46 +0100

    integ-tests: update clck version
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- tests/integration-tests/tests/intel_hpc/test_intel_hpc/test_intel_hpc/install_clck.sh
+++ tests/integration-tests/tests/intel_hpc/test_intel_hpc/test_intel_hpc/install_clck.sh
@@ -4,4 +4,4 @@ set -e
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 sudo yum-config-manager --add-repo https://yum.repos.intel.com/clck/2019/setup/intel-clck-2019.repo
 sudo yum-config-manager --add-repo https://yum.repos.intel.com/clck-ext/2019/setup/intel-clck-ext-2019.repo
-sudo yum -y install intel-clck-2019.3.5-025
+sudo yum -y install intel-clck-2019.6-038
